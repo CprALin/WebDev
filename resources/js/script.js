47e7abcbb4d1c4570382116b143ad5c5bb83e0f5
CprAlin <stfalin@yahoo.com>
2023-05-04 16:31:46 +0300

    new js function

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,6 +3,15 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btn = document.querySelector('.logInBtn');
 const icon = document.querySelector('.icon-close');
+let title = document.title
+
+window.addEventListener('blur', () => {
+     document.title = 'Come Back :(';
+});
+
+window.addEventListener('focus', () => {
+    document.title = title;
+});
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -19,3 +28,4 @@ btn.addEventListener('click', () => {
 icon.addEventListener('click', () => {
    wrapper.classList.remove('activeBtn');
 });
+
